4c290ef12547cf6746f7c3cd126df1b4657b5b41
Raul Giron (he/him/his) <raulgiron@hotmail.com>
2022-06-21 13:37:11 -0400

    Table Extraction from Websites

---
+++ TableExtractionFromWebsites.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+simpsons = pd.read_html("https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)")
+
+
+print(f"{len(simpsons)}\n")
+print(simpsons[1])
+
